c6934de319a627985091dfdb00b9db1937f36f45
Taras Ivashchuk <tvishy@disroot.org>
2026-03-19 23:03:46 +0200

    chore: add return annotation type

--- repositories/redis_repository.py
+++ repositories/redis_repository.py
@@ -10,7 +10,7 @@ class RedisRepository:
     async def save(self, user_id: int, item: Any) -> None:
         await self._connection.set(f"{self._prefix}:{str(user_id)}", item)
 
-    async def load(self, user_id: int):
+    async def load(self, user_id: int) -> Any:
         return await self._connection.get(f"{self._prefix}:{str(user_id)}")
 
     async def close(self) -> None:
