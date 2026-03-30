0291cfc68338e921eff41d92d70d9e6a29386247
Taras Ivashchuk <tvishy@disroot.org>
2026-03-30 18:16:31 +0300

    fix: add dependencies for rag service

--- factory.py
+++ factory.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from chromadb import AsyncClientAPI
+from chromadb.api.models.AsyncCollection import AsyncCollection
 from pydantic_ai.models import Model
 from httpx import AsyncClient
 from pydantic_ai.models.mistral import MistralModel
@@ -10,7 +11,8 @@ from pydantic_ai.providers.mistral import MistralProvider
 
 from agent.interfaces import AgentInterface
 from agent.pydantic_agent import PydanticAgent
-from repositories.interfaces import RepositoryInterface
+from repositories.chroma_repository import ChromaRepository
+from repositories.interfaces import RepositoryInterface, VectorDbInterface
 from redis.asyncio import Redis
 
 from repositories.redis_repository import RedisRepository
@@ -43,8 +45,20 @@ def get_db_client() -> Any:
     return Redis(host=settings.DB_HOST, port=settings.DB_PORT)
 
 
+async def get_vector_db_client() -> Any:
+    return await chromadb.AsyncHttpClient()
+
+
+async def get_vector_db_collection(client: AsyncClientAPI) -> AsyncCollection:
+    return await client.get_or_create_collection(name=settings.VECTOR_DB_COLLECTION_NAME)
+
+
 def get_repository(db_client: Any, db_prefix: str) -> RepositoryInterface:
     return RedisRepository(redis_client=db_client, prefix=db_prefix)
+
+
+def get_vector_repository(db_client: AsyncClientAPI, collection: AsyncCollection):
+    return ChromaRepository(db=db_client, collection=collection)
 
 
 def get_service(
@@ -55,14 +69,25 @@ def get_service(
     return ChatService(agent=agent, repository=repository, rag_service=rag_service)
 
 
-def get_rag_service(settings_repository: RepositoryInterface) -> RagServiceInterface:
-    return RagService(settings_repository=settings_repository)
+def get_rag_service(
+    settings_repository: RepositoryInterface,
+    vector_repository: VectorDbInterface,
+    history_repository: RepositoryInterface,
+    agent: AgentInterface
+
+) -> RagServiceInterface:
+    return RagService(
+        settings_repository=settings_repository,
+        vector_repository=vector_repository,
+        history_repository=history_repository,
+        agent=agent
+    )
 
 
 def get_bot() -> Bot:
     bot = Bot(
         token=settings.BOT_TOKEN,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        default=DefaultBotProperties(parse_mode=None),
     )
 
     return bot
